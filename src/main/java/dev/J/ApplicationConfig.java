9ecbb0627b06c5dd2c95f3838f7d9ac3a7175b2e
SM <jsantamarina919@gmail.com>
2025-11-28 16:32:47 -0800

    added jspecify annotation to package and session factory bean

--- src/main/java/dev/J/ApplicationConfig.java
+++ src/main/java/dev/J/ApplicationConfig.java
@@ -1,9 +1,35 @@
 package dev.J;
 
 
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernatePersistenceConfiguration;
+import org.hibernate.tool.schema.Action;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "dev.J")
 public class ApplicationConfig {
+
+    private static final String DATABASE_URL = "DATABASE_URL";
+
+    private static final String DATABASE_USERNAME = "DATABASE_USERNAME";
+
+    private static final String DATABASE_PASSWORD = "DATABASE_PASSWORD";
+
+
+    @Bean
+    SessionFactory sessionFactory(){
+        var factory = new HibernatePersistenceConfiguration("PlanUni")
+                .jdbcUrl(System.getenv(DATABASE_URL))
+                .jdbcCredentials(System.getenv(DATABASE_USERNAME),System.getenv(DATABASE_PASSWORD))
+                .schemaToolingAction(Action.CREATE_ONLY)
+                .showSql(true,true,true)
+                .createEntityManagerFactory();
+
+        factory.getSchemaManager().create(true);
+
+        return factory;
+    }
+
 
 }
