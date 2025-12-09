697bfb417e2b0ef3ba9e41be5c8cefaf7eba2c39
SM <jsantamarina919@gmail.com>
2025-12-08 17:44:54 -0800

    add supported endpoint

--- src/main/java/dev/J/SchedulerEndpoint.java
+++ src/main/java/dev/J/SchedulerEndpoint.java
@@ -1,4 +1,32 @@
 package dev.J;
 
+import dev.J.Entities.Campus;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class SchedulerEndpoint {
+
+    private final SessionFactory sessionFactory;
+
+    //Hold ids of supported universities in a hardcoded list. However, in the future all CSUs will be supported.
+    private static final List<String> supportedUniversities = List.of("DominguezHills","Bakersfield");
+
+    @GetMapping("/scheduler/universities/supported")
+    public List<Campus> supportedUniversities() {
+        return supportedUniversities
+                .stream()
+                .map(id ->
+                        sessionFactory.fromSession(
+                                session -> session.find(Campus.class,id)
+                        )
+                )
+                .toList();
+    }
+
 }
