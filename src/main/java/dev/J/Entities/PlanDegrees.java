559320082c96ad22f7a860e2d62c1c9c08e348c0
SM <jsantamarina919@gmail.com>
2025-12-23 17:26:19 -0800

    add getter annotation and course state class

--- src/main/java/dev/J/Entities/PlanDegrees.java
+++ src/main/java/dev/J/Entities/PlanDegrees.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @Entity
 public class PlanDegrees {
 
